750939921566405afd8551f586d0d7baae105593
daohainam <daohainam@gmail.com>
2024-05-08 14:41:39 +0200

    Add IsExportFromQuery property

--- DBExporter.Options/DatabaseOptions.cs
+++ DBExporter.Options/DatabaseOptions.cs
@@ -6,5 +6,6 @@
         public string SelectQuery { get; set; } = string.Empty;
         public string TableNames { get; set; } = string.Empty; // a comma-separated table list
         public ServerTypes ServerType { get; set; } = ServerTypes.SqlServer;
+        public bool IsExportFromQuery => string.IsNullOrEmpty(SelectQuery);
     }
 }
